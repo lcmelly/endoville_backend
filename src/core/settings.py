bd53ac24eb6352e76d48c6a5e689974a0fdcb7e3
sezfabian <cheruiyotfabian@gmail.com>
2026-01-02 00:35:08 +0000

    updated allowed hosts

--- src/core/settings.py
+++ src/core/settings.py
@@ -26,7 +26,9 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
+if not ALLOWED_HOSTS:
+    ALLOWED_HOSTS = ['*']
 
 # Change auth user
 AUTH_USER_MODEL = 'users.CustomUser'
